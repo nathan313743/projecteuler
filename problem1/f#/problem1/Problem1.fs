aa288f0e96f3d5bb9b05d0edd0ed18fd395eff1e
nathan313743 <nathandevelopmain@gmail.com>
2017-09-14 12:34:34 +0100

    Removed unused function

--- problem1/f#/problem1/Problem1.fs
+++ problem1/f#/problem1/Problem1.fs
@@ -1,8 +1,4 @@
 namespace Problem1
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
-
 module Problem1 =
     let result = [1..999] |> List.map (fun i -> if i % 5 = 0 || i % 3 = 0 then i else 0) |> List.sum
